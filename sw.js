24f6eabb210fb1521b59989b314884b0bed33662
pradum-gk-ai <tiwaripardum1008@gmail.com>
2026-09-17 08:58:07 +0530

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = "pradum-gk-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
